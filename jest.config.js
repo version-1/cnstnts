a943935de1be9ec9fb8495c9d29f874fd7ab54e2
jiro <j.code.public@gmail.com>
2020-01-22 00:53:42 -0800

    update config

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,10 @@ module.exports = {
     '^.+\\.ts?$': 'ts-jest'
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base)'
+    "<rootDir>/node_modules/",
+  ],
+  modulePathIgnorePatterns: [
+    "<rootDir>/dist/"
   ],
   globals: {
     'ts-jest': {
